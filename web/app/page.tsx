e9d7a336268ba24b29eb56dcbd82c5dcb3d89cc9
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-09 20:30:47 +0200

    update layout.tsx and page.tsx - started working on `homepage`

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,102 +1,62 @@
+'use client'
 import Image from "next/image";
+import { Footer, Header } from "./components";
+import Link from "next/link";
+import { Button } from "@carbon/react";
 export const runtime = "edge";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Header activeLink={"home"}></Header>
+      <div className="">
+        <main className="flex min-h-screen">
+          <section className={'flex-1 flex flex-col'}>
+            <div className={'flex-1 flex justify-center items-center'}>
+              <div className={"p-10 ml-auto -mr-32"}>
+                <p className="font-display text-display-large">Welcome to <br />
+                  Maki Creative Studio</p>
+                <p className="font-headlines text-headline-large uppercase  bg-white dark:bg-offWhite p-2 z-10 relative">Connecting you with your customers</p>
+                <div className="flex  bg-white dark:bg-offWhite p-2 z-10 relative">
+                  <Link href={'/portfolio'}>
+                    <Button className="bg-secondary p-2.5 pl-4 pr-16 hover:bg-secondaryRegal text-white dark:text-offWhite font-label text-label-large font-bold">See Our Work
+                    </Button>
+                  </Link>
+                  <div className="flex-1"></div>
+                  <Link href={'/work'} >
+                    <Button className="bg-primary1B p-2.5 pl-4 pr-16 hover:bg-primary text-white dark:text-offWhite font-label text-label-large font-bold">Let's Create together
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className={"flex-1 border-t-2 border-t-black dark:border-t-offWhite"}>
+              <p className={"p p-16 font-headlines text-headline-medium"}>
+                At Maki Studio, we're not just designers –
+                we're visual problem solvers. Whether you
+                need a stunning brand identity, an intuitive
+                user interface, or scroll-stopping social
+                media graphics, we've got you covered.
+                <br />
+                <br />
+                Our team brings big ideas to life, one pixel
+                at a time
+              </p>
+            </div>
+          </section>
+          <section className={'flex-1 flex justify-center items-center border-l-2 border-l-black dark:border-l-offWhite'}>
+            <div className={"p-10"}>
+              <p className={"font-headlines text-headline-large uppercase"}>Where Creativity meets purpose</p>
+              <div className="h-64 ">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+              </div>`
+              <p className={" font-headlines text-headline-large uppercase"}>We turn Ideas into <br/> visual masterpieces</p>
+            </div>
+          </section>
+        </main>
+
+      </div>
+      <Footer />
+    </>
   );
 }
