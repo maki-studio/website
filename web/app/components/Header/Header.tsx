8e96196a271207e182c120e475dcaa21fb1469d0
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-10 16:40:15 +0200

    update Header.tsx - made header responsive to small screens & large screens

--- web/app/components/Header/Header.tsx
+++ web/app/components/Header/Header.tsx
@@ -15,12 +15,12 @@ export function Header({ activeLink, hTopColor = 'bg-primary1B', hBottomColor = 
   const linkStyling: string = 'hover:text-primary1B hover:duration-150';
 
   return <header>
-    <div className='h-32 px-36 bg-primary0 text-white dark:text-offWhite flex items-center'>
-      <div id="logo">
+    <div className='min-h-32  grid-cols-1 md:grid-cols-6 items-center md:h-32 md:px-16 bg-primary0 text-white dark:text-offWhite grid'>
+      <div id="logo " className='mx-auto md:mx-0 p-4'>
         <Image className='h-20' src={primaryLogo} alt="primary-logo" />
       </div>
-      <nav id="navbar-default" className={'hidden  flex-1 w-full md:block md:w-auto'}>
-        <ul className="mx-14 font-headlines text-headline-large uppercase flex  justify-between flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
+      <nav id="navbar-default" className={'p-4 flex-1 w-full md:w-auto md:col-span-4 border-t-2 md:border-t-0  border-t-white dark:border-t-offWhite'}>
+        <ul className="mx-8 md:mx-14 font-headlines text-headline-large uppercase flex  justify-between flex-col p-4 md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
           <li><Link href={'/'} className={[linkStyling, (activeLink === 'home' ? activeLinkStyling : '')].join(' ')}>Home</Link></li>
           <li><Link href={'/portfolio/'} className={[linkStyling, (activeLink === 'portfolio' ? activeLinkStyling : '')].join(' ')}>Portfolio</Link></li>
           <li><Link href={'/lets-work/'} className={[linkStyling, (activeLink === 'lets-work' ? activeLinkStyling : '')].join(' ')}>Let&apos;s Work</Link></li>
