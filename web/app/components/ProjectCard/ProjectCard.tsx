24c69d4c1363be2280bf2f77e3c19507b8f047bd
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-10 22:55:34 +0200

    update ProjectCard.tsx - finished project card rough draft

--- web/app/components/ProjectCard/ProjectCard.tsx
+++ web/app/components/ProjectCard/ProjectCard.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-
+import Link from "next/link";
+// import Image from 'next/image';
+// import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import { UrlObject } from 'url';
+// import { Link } from '@carbon/react';
 // import styles from './ProjectCard.css';
 
 export interface ProjectCardProps {
-  prop?: string;
+  link?: string | UrlObject;
+  imgSrc: string;
+  alt?: string;
+  title: string;
+
 }
 
-export function ProjectCard({prop = 'default value'}: ProjectCardProps) {
-  return <div className={'styles.ProjectCard'}>ProjectCard {prop}</div>;
+export function ProjectCard({ link = 'default value', imgSrc, title }: ProjectCardProps) {
+  return <div className={''}>
+    <div className='mb-2'>
+      <img src={imgSrc} className='w-auto h-64' alt='' />
+    </div>
+    <p className='mb-1 font-medium font-headlines text-headline-large uppercase'>
+      {title}
+    </p>
+    <div>
+      <div className='flex'>
+        <Link href={link} className='flex text-primary border-b-2 border-b-primary sm:border-b-primary1B font-headlines text-headline-small uppercase  hover:text-white hover:bg-primary1B hover:shadow hover:duration-150'>
+          <span>Explore Project</span>
+          <span>
+            <svg className='h-4' xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="currentColor" d="m18 6l-1.43 1.393L24.15 15H4v2h20.15l-7.58 7.573L18 26l10-10z" /></svg>
+          </span>
+        </Link>
+      </div>
+    </div>
+  </div>;
 }
