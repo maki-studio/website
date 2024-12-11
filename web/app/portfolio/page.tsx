4d9baa4722a9dc002b8cdecd82a1b212986da8ee
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-11 11:46:08 +0200

    update page.tsx - still in progress,..

--- web/app/portfolio/page.tsx
+++ web/app/portfolio/page.tsx
@@ -23,22 +23,24 @@ export default function Page() {
     <main className="min-h-screen flex flex-col md:flex-row">
       <section id="leftBar" className="md:flex flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-10 md:h-screen  md:w-16 md:border-r-2 border-black dark:border-offWhite">
         <div className="hidden md:block h-16 border-b-2 border-black dark:border-offWhite place-content-center ">
-          <Image src={dTriangleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image>
+          <button id="triangleBtn" className="h-full w-full border-0 hover:bg-offWhite hover:p-4 duration-200 hover:invert"><Image src={dTriangleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button>
         </div>
         <div className="flex-1 flex place-content-center">
           <p className="font-display text-display-large m-auto md:-rotate-90 text-nowrap ">Our Creative Playground</p>
         </div>
         <div className="hidden md:block h-16 border-t-2 border-black dark:border-offWhite place-content-center">
-          <Image src={circleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image>
+          <button id="circleBtn" className="h-full w-full border-0 hover:bg-offWhite hover:p-4 duration-200 hover:invert"><Image src={circleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button>
         </div>
       </section>
       <section id="rightPanel" className="flex-1 flex flex-col">
-        <div className={"h-10 md:h-16 border-b md:border-b-2 border-black dark:border-offWhite flex "}>
-          <div className="w-full inline-flex flex-nowrap overflow-hidden space-x-4 animate-infinite-scroll">
-            <p className="font-display text-display-large m-auto text-nowrap ">Here&apos;s a taste of what happens when passion meets pixels</p>
-            <Image src={circleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image>
-
+        <div id="carouselContent" className={"h-10 md:h-16 border-b md:border-b-2 border-black dark:border-offWhite flex "}>
+          <div id="" className="w-full inline-flex flex-nowrap overflow-hidden space-x-4 ">
+            <div className="flex items-center justify-center md:justify-start animate-infinite-scroll">
+              <p className="font-display text-display-large m-auto text-nowrap ">Here&apos;s a taste of what happens when passion meets pixels</p>
+              <Image src={circleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image>
+            </div>
           </div>
+          
         </div>
         <div id="panelContent" className={"flex-1 grid grid-cols-1 md:grid-cols-3"}>
           {projects}
