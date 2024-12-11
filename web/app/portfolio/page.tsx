4774fb83a373da70cd772bd05b539c76504ef1ad
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-11 15:38:32 +0200

    update page.tsx - fixed an untracked broken change

--- web/app/portfolio/page.tsx
+++ web/app/portfolio/page.tsx
@@ -36,15 +36,17 @@ export default function Page() {
       </div>
     </div>
     <main className="min-h-screen flex flex-col md:flex-row">
-    <section id="leftBar" className="md:flex flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-10 md:h-screen  md:w-16 md:border-r-2 border-black dark:border-offWhite">
+    <section id="leftBar" className="flex md:flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-14 md:h-screen md:w-16 border-y md:border-y-0 md:border-r-2  border-black dark:border-offWhite">
         <div className="hidden md:block h-16 border-b-2 border-black dark:border-offWhite place-content-center ">
-          <button id="triangleBtn" className="h-full w-full border-0 hover:bg-offWhite hover:p-4 duration-200 hover:invert"><Image src={dTriangleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button>
+          <button id="triangleBtn" className="h-full w-full border-0 hover:bg-offWhite hover:p-4 duration-200 hover:invert">
+            <Image src={dTriangleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button>
         </div>
         <div className="flex-1 flex place-content-center">
           <p className="font-display text-display-large m-auto md:-rotate-90 text-nowrap ">Our Creative Playground</p>
         </div>
         <div className="hidden md:block h-16 border-t-2 border-black dark:border-offWhite place-content-center">
-          <button id="circleBtn" className="h-full w-full border-0 hover:bg-offWhite hover:p-4 duration-200 hover:invert"><Image src={circleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button>
+          <button id="circleBtn" className="h-full w-full border-0 hover:bg-offWhite hover:p-4 duration-200 hover:invert">
+            <Image src={circleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button>
         </div>
       </section>
       <section id="rightPanel" className="flex-1 flex flex-col">
