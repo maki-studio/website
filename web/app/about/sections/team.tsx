985f2bfad2136004251e1fcadc6a6006d5d317e8
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-13 14:54:44 +0200

    update team.tsx - styled content

--- web/app/about/sections/team.tsx
+++ web/app/about/sections/team.tsx
@@ -2,8 +2,16 @@
 
 export default function TeamSection() {
     return <>
-        <main>
-            The Team section
+        <main className="p-8 flex justify-center">
+            <div className="">
+                <p className=" md:w-[50%]">
+                    Maki Creative Studio isn&apos;t just a name – it&apos;s a promise. A promise to deliver designs that don&apos;t just look pretty, but  solve real problems to connect you with your customers .
+                </p>
+                <br />
+                <p className="md:w-[50%]">
+                    Founded by M. Shoko, our team brings together a perfect blend of creativity, technical know-how, and pragmatism.
+                </p>
+            </div>
         </main>
     </>
 }
