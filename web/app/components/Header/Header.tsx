5aaf766d014e88e276661397e0d27972734ecfad
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-02-05 23:09:34 +0200

    update Header.tsx, fixed alignment & links

--- web/app/components/Header/Header.tsx
+++ web/app/components/Header/Header.tsx
@@ -15,17 +15,15 @@ export function Header({ activeLink, hTopColor = 'bg-primary1B', hBottomColor = 
   const linkStyling: string = 'hover:text-primary1B hover:duration-150';
 
   return <header>
-    <div className='min-h-32  grid-cols-1 md:grid-cols-6 items-center md:h-32 md:px-16 bg-primary0 text-white dark:text-offWhite grid'>
-      <div id="logo " className='mx-auto md:mx-0 p-4'>
-        <Image className='h-20' src={primaryLogo} alt="primary-logo" />
-      </div>
-      <nav id="navbar-default" className={'p-4 flex-1 w-full md:w-auto md:col-span-4 border-t-2 md:border-t-0  border-t-white dark:border-t-offWhite'}>
-        <ul className="mx-8 md:mx-14 font-headlines text-headline-large uppercase flex  justify-between flex-col p-4 md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
+    <div className='min-h-32 items-center md:h-32 md:px-16 bg-primary0 text-white dark:text-offWhite '>
+      <nav id="navbar-default" className={'p-4'}>
+        <ul className="font-headlines text-headline-small font-bold uppercase p-4 md:p-0 flex flex-col md:flex-row space-y-4 md:space-y-0 md:w-min  md:mx-auto items-center md:space-x-8 rtl:space-x-reverse md:mt-0 ">
+          <li className='w-36'><Link href={'/'} className={"w-10"}><Image className='h-20' src={primaryLogo} alt="primary-logo" /></Link></li>
           <li><Link href={'/'} className={[linkStyling, (activeLink === 'home' ? activeLinkStyling : '')].join(' ')}>Home</Link></li>
           <li><Link href={'/portfolio/'} className={[linkStyling, (activeLink === 'portfolio' ? activeLinkStyling : '')].join(' ')}>Portfolio</Link></li>
-          <li><Link href={'/lets-work/'} className={[linkStyling, (activeLink === 'lets-work' ? activeLinkStyling : '')].join(' ')}>Let&apos;s Work</Link></li>
+          <li><Link href={'/lets-work/'} className={[linkStyling, (activeLink === 'lets-work' ? activeLinkStyling : '')].join(' ')}>Let&apos;s&nbsp;Work</Link></li>
           <li><Link href={'/contact/'} className={[linkStyling, (activeLink == 'contact' ? activeLinkStyling : '')].join(' ')}>Contact</Link></li>
-          <li><Link href={'/about/'} className={[linkStyling, (activeLink == 'about' ? activeLinkStyling : '')].join(' ')}>About Us</Link></li>
+          <li><Link href={'/about/'} className={[linkStyling, (activeLink == 'about' ? activeLinkStyling : '')].join(' ')}>About&nbsp;Us</Link></li>
         </ul>
       </nav>
     </div>
