f0b7dfd5debd9c267e6fdd8b84ab77fd4f0d4769
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-02-06 09:11:06 +0200

    update page.tsx, about to check tailwind update

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -19,17 +19,71 @@ export default function Home() {
           </section>
           <section id="process" className={""}>
             <h2 className="h2 font-display text-5xl mb-4 md:mb-8">Simple steps to a stunning design</h2>
+            <div className={"bg-black flex justify-center my-12"}>
+              <Link href={"/form"} className="p-3 px-20 uppercase font-headlines font-bold text-headline-large bg-gradient-to-r bg-opacity-[26] from-[#e79ae7] via-[#ecd179]  to-[#80e8ec] border border-black hover:text-gray1 hover:transition-500 hover:bg-gradient-to-r bg-opacity-[26] hover:from-[#7fecf0] hover:via-[#ecd179]  hover:to-[#85eca6] hover:shadow">
+                lets discuss your vision
+              </Link>
+            </div>
+            <hr className="border-black dark:border-offWhite1B" />
+
           </section>
-          <section id="benefits" className={""}>
-            <h2 className="h2 font-display text-5xl mb-4 md:mb-8">Why choose us?</h2>
+          <section id="benefits" className={"pt-12"}>
+            <div className="p-8 ">
+              <h2 className="h2 font-display text-secondary font-bold text-5xl mb-4 md:mb-8 mt-12">Why choose us?</h2>
+              <div id="gradients p-8 bg-black">
+
+                <div className="border-offWhite p-4 bg-gradient-to-r from-cyan-500 to-blue-500">I</div>
+                <div className="border-offWhite p-4 bg-linear-to-t from-sky-500 to-indigo-500">II</div>
+                <div className="border-offWhite p-4 bg-linear-to-bl from-violet-500 to-fuchsia-500">III</div>
+                <div className="border-offWhite p-4 bg-linear-65 from-purple-500 to-pink-500">IV</div>
+                <br />
+                <br />
+                <div className="border-offWhite p-4 size-18 rounded-full bg-radial from-pink-400 from-40% to-fuchsia-700"></div>
+                <div className="border-offWhite p-4 size-18 rounded-full bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90%"></div>
+                <div className="border-offWhite p-4 size-18 rounded-full bg-radial-[at_25%_25%] from-white to-zinc-900 to-75%"></div>
+                <br />
+                <br />
+                <div className="size-24 rounded-full bg-conic from-blue-600 to-sky-400 to-50%"></div>
+                <div className="size-24 rounded-full bg-conic-180 from-indigo-600 via-indigo-50 to-indigo-600"></div>
+                <div className="size-24 rounded-full bg-conic/decreasing from-violet-700 via-lime-300 to-violet-700"></div>
+                <br />
+                <br />
+                <div className="bg-linear-to-r/srgb from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/hsl from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/oklab from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/oklch from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/longer from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/shorter from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/increasing from-indigo-500 to-teal-400"></div>
+                <div className="bg-linear-to-r/decreasing from-indigo-500 to-teal-400"></div>
+
+              </div>
+            </div>
+
+            <hr className="border-black dark:border-offWhite1B" />
+            <div className={"flex justify-center my-12"}>
+              <div className="flex items-center">
+                <span className="relative h-28 w-24 ">
+                  <Image src={"/images/site/paul-rand.png"} alt={"paul rand portrait"} layout="fill" className="bg-gray-300 bg-opacity-40 object-cover rounded-[180px]"></Image>
+                </span>
+                <div className=" p-8">
+                  <span className=" font-normal font-body leading-tight">&ldquo;<b>Design is the silent ambassador of your brand.</b>&rdquo;
+                    <br />
+                  </span>
+                  <span className="text-title-medium font-medium font-title">— Paul Rand, Legendary Designer
+                  </span>
+                </div>
+              </div>
+            </div>
+            <hr className="border-black dark:border-offWhite1B" />
           </section>
-          <section id="testimonials" className={""}>
-            <h2 className="h2 font-display text-secondaryRegal text-5xl px-8 mb-4 md:mb-8 mt-12">Here’s what others are saying
-              <br/>about us</h2>
+          <section id="testimonials" className={"pt-12"}>
+            <h2 className="h2 font-display text-secondaryRegal font-bold text-5xl px-8 mb-4 md:mb-8 mt-12">Here’s what others are saying
+              <br />about us</h2>
             <div className={"grid grid-cols-2 md:grid-cols-6 border-y border-y-black dark:border-y-offWhite1B"}>
               <div className={"border-r border-r-black dark:border-r-offWhite1B"}></div>
               <div className={"border-r border-r-black dark:border-r-offWhite1B relative"}>
-                <Image src={"/images/site/telephone.png"} alt={"max pete portrait"} layout="fill" className="bg-green-300 object-cover"></Image>
+                <Image src={"/images/site/telephone.png"} alt={"max pete portrait"} layout="fill" className="bg-gray-300 bg-opacity-40 object-cover"></Image>
               </div>
 
               <div className={"border-r border-r-black dark:border-r-offWhite1B"}>
@@ -43,7 +97,7 @@ export default function Home() {
               </div>
 
               <div className={"border-r border-r-black dark:border-r-offWhite1B relative"}>
-                <Image src={"/images/site/telephone.png"} alt={"lisa chen portrait"} layout="fill" className="bg-green-300 object-cover"></Image>
+                <Image src={"/images/site/telephone.png"} alt={"lisa chen portrait"} layout="fill" className="bg-gray-300 bg-opacity-40 object-cover"></Image>
               </div>
               <div className={"border-r border-r-black dark:border-r-offWhite1B"}>
                 <div className=" p-8">
@@ -57,7 +111,7 @@ export default function Home() {
               <div className={"border-r border-r-black dark:border-r-offWhite1B"}></div>
             </div>
             <div className={"bg-black flex justify-center my-12"}>
-              <Link href={"/contact"} className="p-3 px-20 uppercase font-headlines font-bold text-headline-large bg-gradient-to-r bg-opacity-[26] from-[#e79ae7] via-[#ecd179]  to-[#80e8ec] border border-black hover:text-gray1 hover:transition-500 hover:bg-gradient-to-r bg-opacity-[26] hover:from-[#7fecf0] hover:via-[#ecd179]  hover:to-[#85eca6] hover:shadow">
+              <Link href={"/form"} className="p-3 px-20 uppercase font-headlines font-bold text-headline-large bg-gradient-to-r bg-opacity-[26] from-[#e79ae7] via-[#ecd179]  to-[#80e8ec] border border-black hover:text-gray1 hover:transition-500 hover:bg-gradient-to-r bg-opacity-[26] hover:from-[#7fecf0] hover:via-[#ecd179]  hover:to-[#85eca6] hover:shadow">
                 lets discuss your vision
               </Link>
             </div>
