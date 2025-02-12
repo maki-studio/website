0f766882cf2078fb061bd4a1a4c0c34715b46ccc
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-02-12 11:58:18 +0200

    update form page.tsx, added styling

--- web/app/form/page.tsx
+++ web/app/form/page.tsx
@@ -4,6 +4,7 @@ import { Header, Footer } from "@/app/components";
 // import circleIcon from '@/app/icons/ui/circle.svg'
 // import dTriangleIcon from '@/app/icons/ui/down-triangle.svg'
 import React from 'react';
+import FAQSection from "../about/sections/faq";
 
 
 
@@ -18,7 +19,7 @@ export default function FormPage() {
     <Header activeLink="contact"></Header>
 
     <main className="min-h-screen flex flex-col md:flex-row">
-      <section id="leftBar" className="flex md:flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-14 md:h-screen md:w-16 border-y md:border-y-0 md:border-r-2  border-black dark:border-off-white">
+      <section id="leftBar" className="flex md:flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-14 md:h-screen md:w-16 border-y md:border-y-0 md:border-r  border-black dark:border-off-white">
         <div className="md:h-16 w-16 md:w-auto border-x md:border-x-0 md:border-b-0 border-black dark:border-off-white place-content-center ">
           {/* <button id="triangleBtn" className="h-full w-full bg-white dark:bg-gray3 relative z-10 border-0 hover:bg-off-white hover:p-4 duration-200 hover:invert">
             <Image src={dTriangleIcon} alt="circle icon" className="h-8 dark:invert m-auto "></Image></button> */}
@@ -26,10 +27,10 @@ export default function FormPage() {
         <div className="flex-1 flex place-content-center">
           <div className="md:hidden w-full inline-flex flex-nowrap overflow-hidden">
             <div className="flex items-center justify-center md:justify-start animate-infinite-scroll md:animate-none">
-              <p className="font-display text-display-large m-auto md:-rotate-90 text-nowrap capitalize">We&apos;re ready to begin when you are.</p>
+              {/* <p className="font-display text-display-large m-auto md:-rotate-90 text-nowrap capitalize">{projectData[0]?.name ?? 'Our Portfolio'}</p> */}
             </div>
           </div>
-          <p className="hidden md:block font-display text-display-large m-auto md:-rotate-90 text-nowrap capitalize">We&apos;re ready to begin when you are.</p>
+          {/* <p className="hidden md:block font-display text-display-large m-auto md:-rotate-90 text-nowrap capitalize">{projectData[0]?.name ?? 'Our Creative Playground'}</p> */}
         </div>
 
         <div className="md:h-16 w-16 md:w-auto border-x md:border-x-0 md:border-t-0 border-black dark:border-off-white place-content-center ">
@@ -40,14 +41,17 @@ export default function FormPage() {
 
       <section id="midPanel" className="flex-1 flex flex-col">
         <div>
+          <div id="emptyDiv" className="h-12 md:h-16 border-b md:border-b border-black dark:border-off-white"></div>
+          <iframe title="booking form" src="https://tally.so/embed/3x47B5?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" className="min-h-screen w-full md:w-[70vw] lg:w-[50vw] p-8 mx-auto bg-off-white1B border-x-2 border-dashed border-x-black/40 dark:border-off-white" allowFullScreen />
+          <hr className="border-black dark:border-off-white1B" />
+          <div id="emptyDiv" className="h-12 md:h-16 border-b md:border-b border-black dark:border-off-white"></div>
 
-          <iframe title="booking form" src="https://tally.so/embed/3x47B5?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" className="min-h-screen w-full md:w-[70vw] lg:w-[50vw] p-8 pt-0 mx-auto" allowFullScreen />
-
+          <FAQSection></FAQSection>
 
         </div>
       </section>
 
-      <section id="rightBar" className="flex md:flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-14 md:h-screen md:w-16 border-y md:border-y-0 md:border-l-2  border-black dark:border-off-white">
+      <section id="rightBar" className="flex md:flex-col sticky top-0 z-10 bg-white dark:bg-gray3 h-14 md:h-screen md:w-16 border-y md:border-y-0 md:border-l-1  border-black dark:border-off-white">
         <div className="md:h-16 w-16 md:w-auto border-x md:border-x-0 md:border-b-0 border-black dark:border-off-white place-content-center ">
           {/* <button id="triangleBtn" className="h-full w-full bg-white dark:bg-gray3 relative z-10 border-0 hover:bg-off-white hover:p-4 duration-200 hover:invert">
             <Image src={starIcon} alt="star icon" className="h-8 dark:invert m-auto "></Image></button> */}
@@ -55,10 +59,10 @@ export default function FormPage() {
         <div className="flex-1 flex place-content-center">
           <div className="md:hidden w-full inline-flex flex-nowrap overflow-hidden">
             <div className="flex items-center justify-center md:justify-start animate-infinite-scroll md:animate-none">
-              <p className="font-display text-display-large m-auto md:-rotate-90 text-nowrap capitalize">We&apos;re ready to begin when you are.</p>
+              {/* <p className="font-display text-display-large m-auto md:-rotate-90 text-nowrap capitalize">{projectData[0]?.type ?? 'Our Portfolio'}</p> */}
             </div>
           </div>
-          <p className="hidden md:block font-display text-display-large m-auto md:rotate-90 text-nowrap capitalize">We&apos;re ready to begin when you are.</p>
+          {/* <p className="hidden md:block font-display text-display-large m-auto md:rotate-90 text-nowrap capitalize">{projectData[0]?.type ?? 'Our Creative Playground'}</p> */}
         </div>
 
         <div className="md:h-16 w-16 md:w-auto border-x md:border-x-0 md:border-t-0 border-black dark:border-off-white place-content-center ">
